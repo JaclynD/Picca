768c3fa36dd9dd75e49903bf62b8a895e61aa403
Sanojunko <>
2017-12-03 18:20:17 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+companies_csv = CSV.readlines("db/restaurants.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Restaurant.create(name: row[1], address: row[2], main_image: row[3], location: row[4], genre: row[5], seats: row[6], budget: row[7], access: row[8], mobile_access: row[9], open: row[10])
+end
