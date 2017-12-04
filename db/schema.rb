36e60b3f2b4c686aff91440f543df0508ad157af
Sanojunko <>
2017-12-04 16:41:38 +0900

    update avatar design

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130095524) do
+ActiveRecord::Schema.define(version: 20171204073103) do
 
   create_table "photos", force: :cascade do |t|
     t.text     "image",         limit: 65535
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(version: 20171130095524) do
     t.string   "nickname",               limit: 255
     t.text     "avatar",                 limit: 65535
     t.string   "group",                  limit: 255
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
+    t.integer  "avatar_file_size",       limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
